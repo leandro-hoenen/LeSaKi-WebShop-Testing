080af20d94712cef94ad3f6c26bb85ed940ef1e0
Leandro Hoenen <leandrohoenen@MBP-von-Leandro.sunrise.box>
2022-05-22 14:46:28 +0200

    implemented add coupon test

--- cypress/integration/shoppingCart.js
+++ cypress/integration/shoppingCart.js
@@ -2,7 +2,9 @@
 
 describe('Shopping Cart Tests ', () => {
     let product
+    let coupon
     let customer
+    let totalPrice
 
     before(() => {
         cy.fixture('product').then((productData) => {
@@ -11,6 +13,13 @@ describe('Shopping Cart Tests ', () => {
         cy.fixture('customer').then((customerData) => {
             customer = customerData
         })
+        cy.fixture('coupon').then((couponData) => {
+            coupon = couponData
+        })
+    })
+
+    before(() => {
+        totalPrice = parseInt(product.order_quantity) * parseFloat(product.price)
     })
 
     beforeEach(() => {
@@ -18,18 +27,26 @@ describe('Shopping Cart Tests ', () => {
     })
 
     it('Add Product to shopping cart', () => {
-        const totalPrice = (parseInt(product.order_quantity) * parseFloat(product.price)).toFixed(2)
         cy.visit(product.url)
         cy.get('#quantity')
             .clear()
             .type('3')
         cy.get('button[type="submit"]').click()
         cy.visit('shopping-cart')
-        // Assertations
         cy.get('#shopping-cart').should('contain', product.name)
         cy.get('#shopping-cart > div > div.col-sm-8.col-xxl-7 > div > div:nth-child(2) > p').should('have.text', 'CHF ' + product.price)
         cy.get('#quantity-shopping-cart').should('have.text', product.order_quantity + ' Stück')
-        cy.get('#subtotal > p:nth-child(2)').should('have.text', 'CHF ' + totalPrice)
+        cy.get('#subtotal > p:nth-child(2)').should('have.text', 'CHF ' + totalPrice.toFixed(2))
+    })
+
+    it('Add Coupon', () => {
+        const discountPrice = (totalPrice - (totalPrice * parseFloat(coupon.value) / 100)).toFixed(2)
+        cy.visit('shopping-cart')
+        cy.get('#coupon')
+            .should('be.empty')
+            .type(coupon.code)
+        cy.get('#coupon-code > button').click()
+        cy.get('#subtotal > p:nth-child(2)').should('have.text', 'CHF ' + discountPrice)
     })
 
     it('Remove Product from shopping cart', () => {
